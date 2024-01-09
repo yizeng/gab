6261f0c6db1d24debcb4eaa7a7d4c5e8768d0b30
Yi Zeng <developer@yizeng.me>
2024-01-09 16:29:21 +0100

    [chi/crud-gorm] Add POSTGRES_LOG_LEVEL

--- chi/crud-gorm/internal/db/postgres.go
+++ chi/crud-gorm/internal/db/postgres.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"fmt"
+	"strings"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 
 	"github.com/yizeng/gab/chi/crud-gorm/internal/config"
 	"github.com/yizeng/gab/chi/crud-gorm/internal/repository/dao"
@@ -16,7 +18,10 @@ func OpenPostgres(conf *config.PostgresConfig) (*gorm.DB, error) {
 		conf.Host, conf.Port, conf.User, conf.Password, conf.DB,
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	gormLogger := createLogger(conf.LogLevel)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: gormLogger,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("gorm.Open -> %w", err)
 	}
@@ -26,4 +31,23 @@ func OpenPostgres(conf *config.PostgresConfig) (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func createLogger(logLevel string) logger.Interface {
+	var l logger.LogLevel
+
+	switch strings.ToLower(logLevel) {
+	case "silent":
+		l = logger.Silent
+	case "error":
+		l = logger.Error
+	case "warn":
+		l = logger.Warn
+	case "info":
+		l = logger.Info
+	default:
+		l = logger.Error
+	}
+
+	return logger.Default.LogMode(l)
 }
